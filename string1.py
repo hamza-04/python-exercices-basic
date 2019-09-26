591ec83a61a88d3cf708995ca1ba654d71da0d7f
hamza-04 <48214055+hamza-04@users.noreply.github.com>
2019-09-26 13:37:01 +0200

    Update string1.py

--- string1.py
+++ string1.py
@@ -25,7 +25,10 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
+    if count < 10:
+        return 'Number of donuts:'+str(count)
+    else:
+        return 'Number of donuts: many'
 
 
 # B. both_ends
@@ -34,8 +37,10 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  if len(s)<2:
+    return ''
+  else:
+    return s[:2]+s[-2:]
 
 
 # C. fix_start
@@ -48,8 +53,9 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  stra=s[0]
+  s0=s[1:]
+  return stra+s0.replace(stra,'*')
 
 
 # D. MixUp
@@ -59,9 +65,9 @@ def fix_start(s):
 #   'mix', pod' -> 'pox mid'
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
-def mix_up(a, b):
-  # +++your code here+++
-  return
+def mix_up(a,b):
+
+  return b[:2]+a[2:]+' '+a[:2]+b[2:]
 
 
 # Provided simple test() function used in main() to print
@@ -84,22 +90,22 @@ def main():
   test(donuts(10), 'Number of donuts: many')
   test(donuts(99), 'Number of donuts: many')
 
-  print
+
   print 'both_ends'
   test(both_ends('spring'), 'spng')
   test(both_ends('Hello'), 'Helo')
   test(both_ends('a'), '')
   test(both_ends('xyz'), 'xyyz')
 
-  
-  print
+
+
   print 'fix_start'
   test(fix_start('babble'), 'ba**le')
   test(fix_start('aardvark'), 'a*rdv*rk')
   test(fix_start('google'), 'goo*le')
   test(fix_start('donut'), 'donut')
 
-  print
+
   print 'mix_up'
   test(mix_up('mix', 'pod'), 'pox mid')
   test(mix_up('dog', 'dinner'), 'dig donner')
